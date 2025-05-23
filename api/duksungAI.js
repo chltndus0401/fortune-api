2320ce6b7210301a5d06349d967d95ed38bfb98b
chltndus0401 <ally5336@duksung.ac.kr>
2025-05-23 09:52:11 +0900

    fix name

--- api/duksungAI.js
+++ api/duksungAI.js
@@ -13,8 +13,10 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  const name = "최수연";
-  const birth = "2005-04-01";
+  const { name, birth }=req.body;
+  if (!name || !birth) {
+    return res.status(400).json({ error: "이름(name)과 생년월일(birth)이 필요합니다"});
+  }
 
   try {
     const today = new Date().toISOString().slice(0, 10);
@@ -24,7 +26,7 @@ export default async function handler(req, res) {
       생년월일: ${birth}
       오늘 날짜: ${today}
 
-      이 사람의 오늘의 운세를 사주풀이 형식으로 알려줘.
+      이 사람의 오늘의 운세와 행운의 아이템을 사주풀이 형식으로 알려줘.
       `;
 
     const result = await ai.models.generateContent({
@@ -32,7 +34,7 @@ export default async function handler(req, res) {
       contents: prompt,
       config: {
         systemInstruction:
-          "당신은 60년 경력의 사주풀이 전문가입니다. 사람들의 오늘 운세를 200글자 이내로 전해주세요. 운세는 정확하기보다는 읽었을 때 즐거울 수 있도록 부정적인 내용은 없어야 합니다. 마지막에는 희망을 전하는 짧은 격언을 덧붙여 주세요.",
+          "당신은 60년 경력의 사주풀이 전문가입니다. 사람들의 오늘 운세를 100글자 이내로 전해주세요. 운세는 정확하기보다는 읽었을 때 즐거울 수 있도록 부정적인 내용은 없어야 합니다. 마지막에 오늘 가지고 있으면 좋은 <오늘의 행운 아이템>을 추천해주세요.",
       },
     });
 
